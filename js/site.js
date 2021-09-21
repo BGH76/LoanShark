6285d7805d4b727b30661572ad5a20d9ac12eb25
BGH76 <75952591+BGH76@users.noreply.github.com>
2021-09-21 10:47:19 -0400

    commit

--- js/site.js
+++ js/site.js
@@ -1,15 +1,10 @@
 document.getElementById("btnCal").addEventListener("click", calculate);
 
-
-
 let totalPrincipal = document.getElementById("totalPrincipal");
 let totalInterest = document.getElementById("totalInterest");
 let totalCost = document.getElementById("totalCost");
 let totalMonthlyPayment = document.getElementById("totalMonthlyPayment");
-
 let results = document.getElementById("results");
-
-
 
 function calculate(){
 
@@ -28,12 +23,30 @@ function calculate(){
 
     // Set app page data
     totalMonthlyPayment.innerHTML = monthlyPayments.toFixed(2);
+    totalPrincipal.innerHTML = loanAmount;
     totalInterest.innerHTML = ((monthlyPayments * payments) - loanAmount).toFixed(2);
     totalCost.innerHTML = (loanAmount + ((monthlyPayments * payments) - loanAmount)).toFixed(2);
 
-    
-
-    
-
-    
+    // Calculate data and populate table
+    let balance = loanAmount;
+    let principal = 0;
+    let interest = 0;
+    let totInterest = 0;
+    let tagString = [];
+    for(let i = 1; i <= payments; i++){
+        interest = balance * r;
+        principal = monthlyPayments.toFixed(2) - interest;
+        balance = balance - principal;
+        totInterest += interest;
+        tagString += `<tr>
+                    <td>${i}</td>
+                    <td>${monthlyPayments.toFixed(2)}</td>
+                    <td>${principal.toFixed(2)}</td>
+                    <td>${interest.toFixed(2)}</td>
+                    <td>${totInterest.toFixed(2)}</td>
+                    <td>${balance.toFixed(2)}</td>
+                    </tr>`;
+    }
+    results.innerHTML = tagString;
+    console.log(tagString);    
 }
